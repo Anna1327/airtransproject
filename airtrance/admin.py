1ed3c0fbdc4486d6abab1f91d0a5b7b33fce5f77
Anna1327 <aoi.yatsu@ya.ru>
2020-04-24 14:33:11 +0300

    lab2 done

--- airtrance/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(Booking)
-admin.site.register(Ticket)
-admin.site.register(Flight)
-admin.site.register(TicketFlight)
-admin.site.register(Airport)
--- airtrance/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AirtranceConfig(AppConfig):
-    name = 'airtrance'
